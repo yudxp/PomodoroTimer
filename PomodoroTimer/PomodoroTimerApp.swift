5e01535c894ea02361e9673eb77ec98af0a16214
yudxp <yudhahamdi@gmail.com>
2022-07-27 10:20:55 +0700

    Initial Commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by Yudha Hamdi Arzi on 27/07/22.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
